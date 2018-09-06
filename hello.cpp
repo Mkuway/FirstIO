796d3fd44fb48badf720a1026dc689881684c3f3
Mkuway <mkuway@mail.csuchico.edu>
2018-09-05 18:27:59 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
-
+//AtUTHOR: Mathew g. Kuway
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi,my name is Mathew. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
